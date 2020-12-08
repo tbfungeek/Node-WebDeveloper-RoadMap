7adecb892f44775761f02d4832038d1e907318cd
linxiaohai <tbfungeek@163.com>
2020-12-08 19:17:17 +0800

    添加登陆鉴权中间件

---
+++ Egg/17_extend_helper_middleware_shedule/config/config.default.js
@@ -0,0 +1,34 @@
+/* eslint valid-jsdoc: "off" */
+
+"use strict";
+
+/**
+ * @param {Egg.EggAppInfo} appInfo app info
+ */
+module.exports = appInfo => {
+  /**
+   * built-in config
+   * @type {Egg.EggAppConfig}
+   **/
+  const config = (exports = {});
+
+  // use for cookie sign key, should change to your own and keep security
+  config.keys = appInfo.name + "_1607422051287_620";
+
+  // add your middleware config here
+  config.middleware = [];
+
+  config.auth = {
+    excludes: [ 'home', '/user', '/login', '/logout' ],
+  };
+
+  // add your user config here
+  const userConfig = {
+    // myAppName: 'egg',
+  };
+
+  return {
+    ...config,
+    ...userConfig
+  };
+};
---
+++ Egg/17_extend_helper_middleware_shedule/libs/plugin/egg-auth/app/middleware/auth.js
@@ -0,0 +1,16 @@
+'use strict';
+
+module.exports = options => {
+  return async (ctx, next) => {
+    const url = ctx.request.url;
+    const user = ctx.session.user;
+    if (!user && !options.excludes.include(url.split("?")[0])) {
+      ctx.body = {
+        status: 1001,
+        errMsg: '用户未登录',
+      };
+    } else {
+      await next();
+    }
+  };
+};
